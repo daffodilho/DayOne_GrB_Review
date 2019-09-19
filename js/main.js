1a088091924e95e0c30d589287ecf4279ff26069
Daffodil Ho <daffodilho@Daffodils-MBP.fanshawec.ca>
2019-09-19 13:14:01 -0400

    made team bio using object in js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
-import Person from './modules/DataObject.js';
+import Team from './modules/DataObject.js';
 
 // this is an IIFE -> Immediately Invoked Function Expression
 // this is the JavaScript Module Pattern
 (() => {
     console.log('fired!');
 
-    console.log(Person);
+    let bioData = document.querySelector('.bio-wrapper').children;
+
+    // populate the children with the data object values
+    // bioData[0].textContent = Person.name;
+    // bioData[1].textContent = Person.role;
+    // bioData[2].textContent = Person.bio;
+
+    // bioData[0].src = Person["avatar"];
+    // bioData[1].textContent = Person["name"];
+    // bioData[2].textContent = Person["role"];
+    // bioData[3].textContent = Person["bio"];
+
+    function parsePersonData() {
+        // this = the element you clicked on (the button)
+        let person = this.textContent;
+
+        // populate the children with the data object values
+        bioData[0].src = `images/${Team[person].avatar}`;
+        bioData[1].textContent = Team[person].name;
+        bioData[2].textContent = Team[person].role;
+        bioData[3].textContent = Team[person].bio;
+    }
+
+    // loop through the object and build some UI
+    for (let person in Team) {
+        console.log(person);
+
+        // create a button for each team member
+        let currentButton = document.createElement('button');
+
+        // add the person's name to the button
+        currentButton.textContent = person;
+        // add an event handler to the button
+        currentButton.addEventListener("click", parsePersonData);
+        // put the button in the team section on index.html
+        document.querySelector('.team').appendChild(currentButton);
+
+        // bioData.nextElementSibling.appendChild(currentButton);
+    }
+    
 })();
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -1,8 +1,39 @@
 // associating array or object
 export default {
-    name: "Jam",
-    role: "Student",
-    height: "5ft",
-    weight: 100,
-    hairColor: "Rainbow"
+    Trevor: {
+        name: "Trevor",
+        role: "Professor",
+        bio: "the sad, sad story of my life",
+        avatar: "olaf.jpeg"
+    },
+
+    Justin: {
+        name: "Justin",
+        role: "Professor / Coordinator",
+        bio: "we're not sure, but probably something evil",
+        avatar: "justin.jpeg"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "Professor",
+        bio: "i love hunny",
+        avatar: "pooh.jpeg"
+    },
+
+    John: {
+        name: "John",
+        role: "Professor",
+        bio: "red pill or blue pill?",
+        avatar: "pan.jpg"
+    },
+
+    Jarrod: {
+        name: "Jarrod",
+        role: "Professor",
+        bio: "I have more than one look!",
+        avatar: "zoolander.jpg"
+    }
+
+
 }
